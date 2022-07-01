cd2d18627e0b7df6655c19b81518148103667335
Vinh Ky <info.vinhky@gmail.com>
2022-07-01 19:09:50 +0700

    First commit

---
+++ QuanLyCuaHang/Model/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHang.Model
+{
+    public class Product
+    {
+        private string name;
+        private double value;
+        private string image;
+
+        public string Name { get => name; set => name = value; }
+        public double Value { get => value; set => this.value = value; }
+        public string Image { get => image; set => image = value; }
+
+        public Product(string name, double value, string image)
+        {
+            Name = name;
+            Value = value;
+            Image = image;
+        }
+
+    }
+}
---
+++ QuanLyCuaHang/View/SanPham.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyCuaHang.View
+{
+    /// <summary>
+    /// Interaction logic for SanPham.xaml
+    /// </summary>
+    public partial class SanPham : UserControl
+    {
+        public SanPham()
+        {
+            InitializeComponent();
+            var product = GetProduct();
+            if (product.Count > 0)
+            {
+                ListViewProducts.ItemsSource = product;
+            }
+        }
+
+        private List<Model.Product> GetProduct()
+        {
+            return new List<Model.Product>()
+            {
+                new Model.Product("Áo thun", 453, "/Anh/4.jpg"),
+                new Model.Product("Áo thun", 250.46, "/Anh/2.jpg"),
+                new Model.Product("Áo thun 3", 567, "/Anh/3.jpg"),
+                new Model.Product("Áo thun 4", 123, "/Anh/4.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/5.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/8.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/7.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/8.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/4.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/2.jpg"),
+                new Model.Product("Áo thun 5", 12.23, "/Anh/7.jpg"),
+            };
+        }
+    }
+}
