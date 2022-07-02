f1728215f6d43e52fdaa2344dd40aebba0f882f8
Vinh Ky <info.vinhky@gmail.com>
2022-07-02 14:23:11 +0700

    LoadMainWindow

--- QuanLyCuaHang/ViewModel/MainViewModel.cs
+++ QuanLyCuaHang/ViewModel/MainViewModel.cs
@@ -4,11 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLyCuaHang.ViewModel
 {
+    
     public class MainViewModel : BaseViewModel
     {
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand LoadViewSanPham { get; set; }
 
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                Main LoginWindow = new Main();
+                LoginWindow.ShowDialog();
+            }
+              );
+
+            //Load view sản phẩm
+            LoadViewSanPham = new RelayCommand<UserControl>((p) => { return true; }, (p) => {
+                Main m = new Main();
+                m.GridViewMain.Children.Clear();
+                m.GridViewMain.Children.Add(new View.SanPham());
+            }
+              );
+        }
     }
 }
