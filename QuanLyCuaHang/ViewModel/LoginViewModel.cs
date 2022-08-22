c7ea6e834d1d28c54faffc68b73a86a93e4c12e1
Vinh Ky <info.vinhky@gmail.com>
2022-08-22 22:03:52 +0700

    Hoàn thành lấy lại mật khẩu bằng Gmail

--- QuanLyCuaHang/ViewModel/LoginViewModel.cs
+++ QuanLyCuaHang/ViewModel/LoginViewModel.cs
@@ -10,11 +10,16 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Net;
+using System.Net.Mail;
+using QuanLyCuaHang.Form;
 
 namespace QuanLyCuaHang.ViewModel
 {
     public class LoginViewModel:BaseViewModel
     {
+        private string _EmailAccount { get; set; }
+        public string EmailAccount { get => _EmailAccount; set { _EmailAccount = value; OnPropertyChanged(); } }
         private string directory = "";
         private string _Username { get; set; }
         public string Username { get=>_Username; set { _Username = value; OnPropertyChanged(); } }
@@ -23,9 +28,11 @@ namespace QuanLyCuaHang.ViewModel
         public bool isLogin = false;
         public ICommand LoginCommand { get; set; }
         public ICommand PasswordChangedCommand { get; set; }
+        public ICommand ForgotPassword { get; set; }
+        public ICommand SendEmail { get; set; }
         public LoginViewModel()
         {
-
+            Random rand = new Random();
             //Lấy đường dẫn nơi chứa Project
             directory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
 
@@ -37,6 +44,51 @@ namespace QuanLyCuaHang.ViewModel
             PasswordChangedCommand = new RelayCommand<PasswordBox>((p) => { return true; }, (p) => 
             {
                 this.Password = p.Password.ToString();
+            });
+
+            ForgotPassword = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                FormForgotEmail formForgotEmail = new FormForgotEmail();
+                formForgotEmail.ShowDialog();
+            });
+
+            SendEmail = new RelayCommand<object>((p) => 
+            {
+                return true; 
+            }, (p) =>
+            {
+                var KtrDangKy = DataProvider.Ins.DB.TaiKhoans.Where(x=>x.NhanVien.Email == EmailAccount).SingleOrDefault();
+                if (KtrDangKy != null)
+                {
+                    int newPass = rand.Next(100000, 999999);
+                    KtrDangKy.Password = MD5Hash(Base64Encode(newPass.ToString()));
+                    DataProvider.Ins.DB.SaveChanges();
+                    try
+                    {
+                        using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                        {
+                            smtp.EnableSsl = true;
+                            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                            smtp.UseDefaultCredentials = false;
+                            smtp.Credentials = new NetworkCredential("vinhky0167@gmail.com", "zthangqlxnqwuobv");
+
+                            MailMessage mail = new MailMessage();
+                            mail.To.Add(EmailAccount);
+                            mail.From = new MailAddress("vinhky0167@gmail.com");
+                            mail.Subject = "LẤY LẠI MẬT KHẨU";
+                            mail.Body = $"Mật khẩu mới của bạn là: {newPass}. Trân trọng !";
+
+                            smtp.Send(mail);
+                            MessageBox.Show($"Đã gửi mật khẩu vào Email {EmailAccount} đã đăng ký", "Thông báo");
+                        }
+                    }
+                    catch { }
+                }
+                else
+                {
+                    MessageBox.Show("Email của bạn chưa đúng vui lòng nhập lại !!!", "Thông báo");
+                    EmailAccount = "";
+                }
             });
         }
 
